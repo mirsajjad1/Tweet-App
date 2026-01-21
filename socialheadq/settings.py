8502b94e7894d0d6e6d5341c9d8126217c141d39
Mir Mohammad Sajjadul Islam <127872800+mirsajjad1@users.noreply.github.com>
2026-01-21 14:56:17 +0600

    Reformat settings.py with consistent indentation

--- socialheadq/settings.py
+++ socialheadq/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure--j^2qr%7fuqvy=dbs*jm!sg7bg09!i#(lwty4h$yx8^2henj!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', "post-app-s32u.onrender.com"]
+ALLOWED_HOSTS = ['*', 'post-app-s32u.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://post-app-s32u.onrender.com",
+    'https://post-app-s32u.onrender.com',
 ]
 
 CSRF_COOKIE_SECURE = True
@@ -147,6 +147,7 @@ LOGIN_REDIRECT_URL = '//'
 
 
 LOGOUT_REDIRECT_URL = '/post/'
+
 
 
 
